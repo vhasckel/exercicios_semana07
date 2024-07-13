534b7f53cda4805d00a912fa27e4e26ab9bb8eac
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-07-13 19:01:16 -0300

    resolução exercício 02

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -4,5 +4,6 @@ const CursoController = require("../controllers/CursoController");
 const cursosRoutes = new Router();
 
 cursosRoutes.post("/", CursoController.criar);
+cursosRoutes.get("/", CursoController.listaTodos);
 
 module.exports = cursosRoutes;
